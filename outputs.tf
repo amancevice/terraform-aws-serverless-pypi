8e88a8f8ea2c3ceeb2266d257a6fe7a35f64f60f
Alexander Mancevice <amancevice@cargometrics.com>
2020-02-19 17:42:14 -0500

    Revert "remove trailing periods"
    This reverts commit b922b492a24f5f177e3a08846aa5fd88651f9d19.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output api {
-  description = "PyPI REST API"
+  description = "PyPI REST API."
   value       = aws_api_gateway_rest_api.api
 }
 
 output bucket {
-  description = "PyPI S3 bucket"
+  description = "PyPI S3 bucket."
   value       = aws_s3_bucket.pypi
 }
 
 output lambda {
-  description = "PyPI REST API Lambda proxy"
+  description = "PyPI REST API Lambda proxy."
   value       = aws_lambda_function.api
 }
 
 output role {
-  description = "PyPI REST API Lambda IAM role"
+  description = "PyPI REST API Lambda IAM role."
   value       = aws_iam_role.role
 }
