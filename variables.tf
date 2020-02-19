b922b492a24f5f177e3a08846aa5fd88651f9d19
Alexander Mancevice <amancevice@cargometrics.com>
2020-02-19 17:38:56 -0500

    remove trailing periods

--- variables.tf
+++ variables.tf
@@ -1,90 +1,90 @@
 variable api_authorization {
-  description = "API Gateway method authorization [NONE | CUSTOM | AWS_IAM | COGNITO_USER_POOLS]."
-  default     = "NONE"
+  description = "API Gateway method authorization [NONE | CUSTOM | AWS_IAM | COGNITO_USER_POOLS]"
+  default     = "NON"
 }
 
 variable api_authorizer_id {
-  description = "API Gateway authorizer ID."
+  description = "API Gateway authorizer ID"
   default     = null
 }
 
 variable api_base_path {
-  description = "PyPI index API Gateway base path."
+  description = "PyPI index API Gateway base path"
   default     = null
 }
 
 variable api_description {
-  description = "API Gateway REST API description."
-  default     = "PyPI service"
+  description = "API Gateway REST API description"
+  default     = "PyPI servic"
 }
 
 variable api_endpoint_configuration_type {
-  description = "API Gateway endpoint configuration type [EDGE | REGIONAL | PRIVATE]."
-  default     = "REGIONAL"
+  description = "API Gateway endpoint configuration type [EDGE | REGIONAL | PRIVATE]"
+  default     = "REGIONA"
 }
 
 variable api_name {
-  description = "API Gateway REST API name."
+  description = "API Gateway REST API name"
 }
 
 variable lambda_description_api {
-  description = "REST API Lambda function description."
-  default     = "PyPI service REST API"
+  description = "REST API Lambda function description"
+  default     = "PyPI service REST AP"
 }
 
 variable lambda_description_reindex {
-  description = "Reindexer Lambda function decription."
-  default     = "Reindex PyPI root"
+  description = "Reindexer Lambda function decription"
+  default     = "Reindex PyPI roo"
 }
 
 variable lambda_function_name_api {
-  description = "REST API Lambda function name."
+  description = "REST API Lambda function name"
 }
 
 variable lambda_function_name_reindex {
-  description = "Reindexer Lambda function name."
+  description = "Reindexer Lambda function name"
 }
 
 variable lambda_memory_size_api {
-  description = "REST API Lambda function memory size."
+  description = "REST API Lambda function memory size"
   default     = 128
 }
 
 variable lambda_memory_size_reindex {
-  description = "Reindexer Lambda function memory size."
+  description = "Reindexer Lambda function memory size"
   default     = 128
 }
 
 variable log_group_retention_in_days {
-  description = "CloudWatch log group retention period."
+  description = "CloudWatch log group retention period"
   default     = 30
 }
 
 variable policy_name {
-  description = "IAM role inline policy name."
-  default     = "pypi-lambda-permissions"
+  description = "IAM role inline policy name"
+  default     = "pypi-lambda-permission"
 }
 
 variable role_description {
-  description = "Lambda functions IAM role description."
-  default     = "PyPI Lambda permissions"
+  description = "Lambda functions IAM role description"
+  default     = "PyPI Lambda permission"
 }
 
 variable role_name {
-  description = "Lambda function role name."
+  description = "Lambda function role name"
 }
 
 variable s3_bucket_name {
-  description = "PyPI index S3 bucket name."
+  description = "PyPI index S3 bucket name"
 }
 
 variable s3_presigned_url_ttl {
-  description = "PyPI package presigned URL expiration in seconds."
+  description = "PyPI package presigned URL expiration in seconds"
   default     = 900
 }
 
 variable tags {
-  description = "Resource tags."
+  description = "Resource tags"
   type        = map
   default     = {}
 }
