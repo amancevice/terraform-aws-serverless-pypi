d369db6b1c1df65c2565f8e0844aab3e42ccdbe4
Alexander Mancevice <amancevice@cargometrics.com>
2020-04-26 20:27:04 -0400

    Output Lambdas and their ARNs

--- outputs.tf
+++ outputs.tf
@@ -13,9 +13,24 @@ output deployment {
   value       = aws_api_gateway_deployment.deployment
 }
 
-output lambda {
-  description = "PyPI REST API Lambda proxy"
+output lambda_api {
+  description = "PyPI REST API proxy Lambda function"
   value       = aws_lambda_function.api
+}
+
+output lambda_api_arn {
+  description = "PyPI REST API proxy Lambda function ARN"
+  value       = local.lambda_api_arn
+}
+
+output lambda_reindex {
+  description = "Reindexer Lambda function"
+  value       = aws_lambda_function.reindex
+}
+
+output lambda_reindex_arn {
+  description = "Reindexer Lambda function ARN"
+  value       = local.lambda_reindex_arn
 }
 
 output role {
