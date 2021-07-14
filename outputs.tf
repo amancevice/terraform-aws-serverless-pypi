a8d20a35dd5947e14a11dc4770535242be274f4c
Alexander Mancevice <alexander.mancevice@hey.com>
2021-07-14 14:22:39 -0400

    update outputs
    output Lambda log groups
    update descriptions

--- outputs.tf
+++ outputs.tf
@@ -4,18 +4,28 @@ output "s3_bucket" {
 }
 
 output "iam_role" {
-  description = "PyPI REST API Lambda IAM role"
+  description = "PyPI API Lambda IAM role"
   value       = aws_iam_role.role
 }
 
 output "lambda_api" {
-  description = "PyPI REST API proxy Lambda function"
+  description = "PyPI API proxy Lambda function"
   value       = aws_lambda_function.api
+}
+
+output "lambda_api_log_group" {
+  description = "PyPI API proxy Lambda function CloudWatch log group"
+  value       = aws_cloudwatch_log_group.api
 }
 
 output "lambda_reindex" {
   description = "Reindexer Lambda function"
   value       = aws_lambda_function.reindex
+}
+
+output "lambda_reindex_log_group" {
+  description = "Reindexer Lambda function CloudWatch log group"
+  value       = aws_cloudwatch_log_group.reindex
 }
 
 output "sns_topic" {
