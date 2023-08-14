44f9b8be949a5238b360af0999216ea270467a9c
Alexander <alexander.mancevice@hey.com>
2023-08-14 08:49:42 -0400

    generate ssl cert in example

--- example/terraform.tf
+++ example/terraform.tf
@@ -14,18 +14,25 @@ provider "aws" {
 
 variable "domain_name" { type = string }
 
-data "aws_acm_certificate" "ssl" {
-  domain   = var.domain_name
-  statuses = ["ISSUED"]
-}
-
 data "aws_route53_zone" "zone" {
   name = var.domain_name
 }
 
+resource "aws_acm_certificate" "pypi" {
+  domain_name       = "pypi.${data.aws_route53_zone.zone.name}"
+  validation_method = "DNS"
+
+  lifecycle { create_before_destroy = true }
+}
+
+resource "aws_acm_certificate_validation" "pypi" {
+  certificate_arn         = aws_acm_certificate.pypi.arn
+  validation_record_fqdns = [for record in aws_route53_record.ssl : record.fqdn]
+}
+
 resource "aws_api_gateway_domain_name" "pypi" {
   domain_name              = "pypi.${var.domain_name}"
-  regional_certificate_arn = data.aws_acm_certificate.ssl.arn
+  regional_certificate_arn = aws_acm_certificate.pypi.arn
 
   endpoint_configuration { types = ["REGIONAL"] }
 }
@@ -35,6 +42,23 @@ resource "aws_api_gateway_base_path_mapping" "pypi" {
   base_path   = aws_api_gateway_stage.simple.stage_name
   domain_name = aws_api_gateway_domain_name.pypi.domain_name
   stage_name  = aws_api_gateway_stage.simple.stage_name
+}
+
+resource "aws_route53_record" "ssl" {
+  for_each = {
+    for dvo in aws_acm_certificate.pypi.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.zone.zone_id
 }
 
 resource "aws_route53_record" "pypi" {
